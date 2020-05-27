3a360ab67fb189c4c4e6933d85881e4c9b821543
Samantha Bartlett <sammynilla@protonmail.com>
2020-05-27 04:13:11 -0400

    removing some files temporarily, house cleaning.

--- debug_error.cmd
+++ debug_error.cmd
@@ -5,28 +5,22 @@ EXIT /b 9999
 
 :DLLNotFound
 SETLOCAL
-::
 SET ERR_MSG="The specified .dll file cannot be found."
 CALL :DebugError %ERR_MSG%
 GOTO :EOF
-::
 ENDLOCAL
 
 :ExportsNotFound
 SETLOCAL
-::
 SET ERR_MSG="The specified export file could not be found."
 CALL :DebugError %ERR_MSG%
 GOTO :EOF
-::
 ENDLOCAL
 
 :DebugError
 SETLOCAL
-::
 REM "Dequoting the error message in preparation for ECHO."
 SET ERR_MSG=%~1
 @ECHO %ERR_MSG%
 GOTO :EOF
-::
 ENDLOCAL
--- msvcrt/msvcrt_aggregate.cpp
+++
@@ -1,9 +0,0 @@
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-    int _fltused;
-    int _tls_index;
-#ifdef __cplusplus
-}
-#endif

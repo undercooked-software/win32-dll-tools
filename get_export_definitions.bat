fa79315eeca385403f3a06c410ea9e89b2a6b1fc
Samantha Bartlett <sammynilla@protonmail.com>
2020-05-27 04:06:55 -0400

    adding msvcrt.dll and the dll dump

--- get_export_definitions.bat
+++ get_export_definitions.bat
@@ -1,36 +1,22 @@
 @ECHO OFF
-CALL env_init.bat
-
 SETLOCAL
-::
 SET EXPORTS_HEADER_LEN=19
 SET EXPORTS_FILE=%~1
 
-IF NOT ^
-EXIST "%EXPORTS_FILE%" %{%
-        CALL :ExportsNotFound
-        GOTO :EOF
-        REM }
-%}%
+IF NOT EXIST "%EXPORTS_FILE%" GOTO :ExportsNotFound
 
-FOR /F "tokens=1,2 delims=." %%a IN ("%EXPORTS_FILE%")^
-DO %{%
-        SET FILENAME=%%a
-        REM }
-%}%
+REM "Split file name at '.' delimiter "
+FOR /F "tokens=1,2 delims=." %%a IN ("%EXPORTS_FILE%") DO (SET FILENAME=%%a)
 
 SET OUTPUT_FILE="%FILENAME%.def"
 
 @ECHO Generating file: %OUTPUT_FILE%
 @ECHO EXPORTS > %OUTPUT_FILE%
-FOR /F "usebackq tokens=4 skip=%EXPORTS_HEADER_LEN% delims= " %%d IN (%EXPORTS_FILE%)^
-DO %{%
-        @ECHO %%d >> %OUTPUT_FILE%
-        REM }
-%}%
+FOR /F "usebackq tokens=4 skip=%EXPORTS_HEADER_LEN% delims= " %%d IN (%EXPORTS_FILE%) DO (
+	@ECHO %%d >> %OUTPUT_FILE%
+)
 
 GOTO :EOF
-::
 ENDLOCAL
 
 :ExportsNotFound

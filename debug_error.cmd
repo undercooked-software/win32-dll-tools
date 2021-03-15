5d08c27d29658b20d70ec6f669275b4cd913fe4c
Samantha Bartlett <sammynilla@protonmail.com>
2021-03-15 01:00:44 -0400

    changed some styling in debug_error.cmd to stay consistent with future changes.

--- debug_error.cmd
+++ debug_error.cmd
@@ -1,26 +1,26 @@
 @ECHO OFF
-
-REM "This file should not be called directly."
-EXIT /b 9999
+ECHO This file contains error handling functionality and should not be ran by itself.
+:: ========== FUNCTIONS ==========
+EXIT /B
 
 :DLLNotFound
 SETLOCAL
-SET ERR_MSG="The specified .dll file cannot be found."
-CALL :DebugError %ERR_MSG%
-GOTO :EOF
+  SET ERR_MSG="The specified .dll file cannot be found."
+  CALL :DebugError %ERR_MSG%
+  GOTO :EOF
 ENDLOCAL
 
 :ExportsNotFound
 SETLOCAL
-SET ERR_MSG="The specified export file could not be found."
-CALL :DebugError %ERR_MSG%
-GOTO :EOF
+  SET ERR_MSG="The specified export file could not be found."
+  CALL :DebugError %ERR_MSG%
+  GOTO :EOF
 ENDLOCAL
 
 :DebugError
 SETLOCAL
-REM "Dequoting the error message in preparation for ECHO."
-SET ERR_MSG=%~1
-@ECHO %ERR_MSG%
-GOTO :EOF
+  REM Dequoting the error message in preparation for ECHO.
+  SET ERR_MSG=%~1
+  ECHO %ERR_MSG%
+  GOTO :EOF
 ENDLOCAL

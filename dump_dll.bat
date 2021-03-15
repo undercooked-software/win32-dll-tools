8a151d70bb77757425db55956e721726b083dc67
Samantha Bartlett <sammynilla@protonmail.com>
2021-03-15 01:02:53 -0400

    This is a work in progress. We're setting up our scripts to be able to work without needing to instantiate vcvars or be ran from within the Visual Studio CLI tools.

--- dump_dll.bat
+++ dump_dll.bat
@@ -1,20 +1,28 @@
 @ECHO OFF
 SETLOCAL
-SET CWD=%~dp0
-SET QUIET=^>nul 2^>nul
-
-SET DLL_FILE=%~1
-IF NOT EXIST "%DLL_FILE%" GOTO :DLLNotFound
-
-FOR /F "tokens=1,2 delims=." %%a IN ("%DLL_FILE%") DO (SET DLL_DIR=%%a)
-FOR /F "delims=" %%a in ("%DLL_DIR%") DO (SET OUTPUT_DIR=%CWD%%%~na)
-MKDIR %OUTPUT_DIR% %QUIET%
-
-REM "If you wish to have a full disassembly, use /ALL instead of /EXPORTS."
-DUMPBIN /NOLOGO /EXPORTS %DLL_FILE% > "%OUTPUT_DIR%\dump.exp"
-GOTO :EOF
+  REM ECHO %%~f0 in dump_dll.bat = %~f0
+  CALL env_init.bat %~f0
+  ECHO %CWD%
+  EXIT /B
+  
+  SET DLL_FILE=%~f1
+  IF NOT EXIST "%DLL_FILE%" (
+    CALL :DLLNotFound
+    EXIT /B
+  )
+  
+  FOR /F "tokens=1,2 delims=." %%a IN ("%DLL_FILE%") DO (SET DLL_DIR=%%a)
+  FOR /F "delims=" %%a in ("%DLL_DIR%") DO (SET OUTPUT_DIR=%CWD%%%~na)
+  MKDIR %OUTPUT_DIR% %QUIET%
+  
+  REM "If you wish to have a full disassembly, use /ALL instead of /EXPORTS."
+  DUMPBIN /NOLOGO /EXPORTS %DLL_FILE% > "%OUTPUT_DIR%\dump.exp"
+  GOTO :EOF
 ENDLOCAL
 
+REM Until we find different way to do this, we'll use the label trick to call our ENV_INIT function from env_init.bat
+:ENV_INIT
+
 :DLLNotFound
-debug_error.cmd %*
-GOTO :EOF
+  debug_error.cmd
+  GOTO :EOF
--- env_init.bat
+++ env_init.bat
@@ -1,10 +1,38 @@
 @ECHO OFF
+IF "%~1" == "" (
+  ECHO A fully qualified path to a parent script is missing.
+  EXIT /B
+)
 
-SET {=( REM )}
-SET }=)
+REM This check would be skipped in the event that the %~1 parameter failed to exist.
+:: Ultimately, we want to make this check be done on a persistent variable containing %%~f of the parent script.
+:: This file would then perform cleanup of that persistent variable after initializing the environment.
+FOR %%? IN (%~1) DO (
+  REM ECHO FQP=%%~f?
+  REM ECHO extension=%%~x?
+  IF NOT "%%~x?" == ".bat" (
+    ECHO The argument provided does not point to a parent script.
+    EXIT /B
+  )
+)
 
-SET QUIET=^>nul 2^>nul
+REM Just in case we CALL env_init.bat, we can grab env_init as the function name
+FOR /F "tokens=1 delims=." %%A IN ("%~0") DO (SET FUNC_NAME=%%A)
+REM Can't call :ENV_INIT via :env_init, so we capitilize the whole thing.
+REM https://www.robvanderwoude.com/battech_convertcase.php describes this approach.
+FOR /F "tokens=2 delims=-" %%A IN ('FIND "" "%FUNC_NAME%" 2^>^&1') DO (
+  SET FUNC=%%A
+  SET "FUNC_NAME="
+)
+SET FUNC=%FUNC: =%
+CALL :%FUNC%
+SET "FUNC="
+EXIT /B
 
-SET BIN_PATH=bin
-SET BIN_PATH_X86=%BIN_PATH%\x86
-SET BIN_PATH_X64=%BIN_PATH%\x64
+:ENV_INIT
+  SET CWD=%~dp0
+  SET QUIET=^>nul 2^>nul
+  
+  SET BIN_PATH=bin
+  SET BIN_PATH_X86=%BIN_PATH%\x86
+  SET BIN_PATH_X64=%BIN_PATH%\x64

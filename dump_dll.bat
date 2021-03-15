1d0cb26441e901f65993e26ab951a694a7867065
Samantha Bartlett <sammynilla@protonmail.com>
2021-03-15 15:58:54 -0400

    Happy with where these scripts currently are. They're all working properly.

--- dump_dll.bat
+++ dump_dll.bat
@@ -1,9 +1,6 @@
 @ECHO OFF
 SETLOCAL
-  REM ECHO %%~f0 in dump_dll.bat = %~f0
   CALL env_init.bat %~f0
-  ECHO %CWD%
-  EXIT /B
   
   SET DLL_FILE=%~f1
   IF NOT EXIST "%DLL_FILE%" (
@@ -14,14 +11,17 @@ SETLOCAL
   FOR /F "tokens=1,2 delims=." %%a IN ("%DLL_FILE%") DO (SET DLL_DIR=%%a)
   FOR /F "delims=" %%a in ("%DLL_DIR%") DO (SET OUTPUT_DIR=%CWD%%%~na)
   MKDIR %OUTPUT_DIR% %QUIET%
+
+  PUSHD %TOOLCHAIN_SETUP_PATH%
+    CALL "setup_cl_generic.bat" x64
+  POPD
   
-  REM "If you wish to have a full disassembly, use /ALL instead of /EXPORTS."
+  REM If you wish to have a full disassembly, use /ALL instead of /EXPORTS.
   DUMPBIN /NOLOGO /EXPORTS %DLL_FILE% > "%OUTPUT_DIR%\dump.exp"
-  GOTO :EOF
 ENDLOCAL
 
-REM Until we find different way to do this, we'll use the label trick to call our ENV_INIT function from env_init.bat
-:ENV_INIT
+:: ========== FUNCTIONS ==========
+EXIT /B
 
 :DLLNotFound
   debug_error.cmd
--- env_init.bat
+++ env_init.bat
@@ -1,38 +1,38 @@
 @ECHO OFF
 IF "%~1" == "" (
-  ECHO A fully qualified path to a parent script is missing.
+  ECHO The path to the parent script is missing.
   EXIT /B
 )
 
-REM This check would be skipped in the event that the %~1 parameter failed to exist.
-:: Ultimately, we want to make this check be done on a persistent variable containing %%~f of the parent script.
-:: This file would then perform cleanup of that persistent variable after initializing the environment.
-FOR %%? IN (%~1) DO (
-  REM ECHO FQP=%%~f?
-  REM ECHO extension=%%~x?
-  IF NOT "%%~x?" == ".bat" (
-    ECHO The argument provided does not point to a parent script.
-    EXIT /B
+:: Ultimately, we want to do a check here on a persistent environment variable containing %%~f of the parent script.
+:: We would do a comparison between %~1 and the %%~f contained in the persistent variable to make sure they are equal.
+:: Doing this would prevent env_init.bat from being called in the command-line with a valid %~1 passed.
+:: This file would then perform cleanup of that persistent environment variable after finishing initialization.
+
+SETLOCAL
+  REM This check would be skipped in the event that the %~1 parameter is empty.
+  FOR %%? IN (%~1) DO (
+    REM ECHO FQP=%%~f?
+    REM ECHO extension=%%~x?
+    SET PARENT_FOUND=1
+    IF NOT "%%~x?" == ".bat"  SET PARENT_FOUND=
+    IF NOT EXIST "%%~f?"      SET PARENT_FOUND=
+    IF NOT DEFINED PARENT_FOUND (
+      ECHO The argument provided does not point to a parent script.
+      EXIT /B
+    )
   )
-)
+ENDLOCAL
 
-REM Just in case we CALL env_init.bat, we can grab env_init as the function name
-FOR /F "tokens=1 delims=." %%A IN ("%~0") DO (SET FUNC_NAME=%%A)
-REM Can't call :ENV_INIT via :env_init, so we capitilize the whole thing.
-REM https://www.robvanderwoude.com/battech_convertcase.php describes this approach.
-FOR /F "tokens=2 delims=-" %%A IN ('FIND "" "%FUNC_NAME%" 2^>^&1') DO (
-  SET FUNC=%%A
-  SET "FUNC_NAME="
-)
-SET FUNC=%FUNC: =%
-CALL :%FUNC%
-SET "FUNC="
+SET CWD=%~dp0
+SET QUIET=^>nul 2^>nul
+
+SET BIN_PATH=bin
+SET BIN_PATH_X86=%BIN_PATH%\x86
+SET BIN_PATH_X64=%BIN_PATH%\x64
+
+REM The majority of our scripts have a dependency on our msvc-no-vcvars compiler toolchain setup.
+REM There is no harm in placing this here as this part only can no longer pollute ENV
+SET TOOLCHAIN_SETUP_PATH="C:\dev_tools\bin\msvc-no-vcvars"
+
 EXIT /B
-
-:ENV_INIT
-  SET CWD=%~dp0
-  SET QUIET=^>nul 2^>nul
-  
-  SET BIN_PATH=bin
-  SET BIN_PATH_X86=%BIN_PATH%\x86
-  SET BIN_PATH_X64=%BIN_PATH%\x64

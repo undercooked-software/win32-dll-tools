ebf4802a2977b18e14d8efa968cbd8ee0cc6ada7
Samantha Bartlett <sammynilla@protonmail.com>
2022-01-24 17:29:38 -0500

    Cleaned up and fixed build_libs.bat. Newly created libs now go to bin/ and msvcrt.dll has been officially removed from the repo.

--- build_libs.bat
+++ build_libs.bat
@@ -1,43 +1,43 @@
 @ECHO OFF
-CALL env_init.bat
-
 SETLOCAL
-::
-CALL :MakeDirectories
+  REM %~f0 is the fully qualified path for the index given that cooresponds to the argument passed via CLI.
+  REM This can be viewed as [drive]:\[path]\[file] for argument index 0.
+  SET PARENT_FILE=%~f0
+  CALL env_init.bat %PARENT_FILE%
 
-SET DEF_FILE=%~1
+  SET DEF_FILE=%~1
+  IF NOT EXIST "%DEF_FILE%" (
+    CALL :ExportsNotFound
+    EXIT /B
+  )
 
-IF NOT ^
-EXIST "%DEF_FILE%" %{%
-        CALL :ExportsNotFound
-        GOTO :EOF
-        REM }
-%}%
+  FOR /F "tokens=1,2 delims=\" %%a IN ("%DEF_FILE%") DO (
+    SET FILENAME=%%a
+  )
 
-FOR /F "tokens=1,2 delims=." %%a IN ("%DEF_FILE%")^
-DO %{%
-        SET FILENAME=%%a
-        REM }
-%}%
+  CALL :MakeDirectories
 
-LIB /NOLOGO /DEF:%~1 /OUT:%BIN_PATH_X86%\%FILENAME%.lib /MACHINE:X86
-LIB /NOLOGO /DEF:%~1 /OUT:%BIN_PATH_X64%\%FILENAME%.lib /MACHINE:X64
+  PUSHD %TOOLCHAIN_SETUP_PATH%
+    CALL "setup_cl_generic.bat" x64
+  POPD
 
-GOTO :Clean
-::
+  LIB /NOLOGO /DEF:%~1 /OUT:%BIN_PATH_X86%\%FILENAME%.lib /MACHINE:X86
+  LIB /NOLOGO /DEF:%~1 /OUT:%BIN_PATH_X64%\%FILENAME%.lib /MACHINE:X64
+
+  GOTO :Clean
 ENDLOCAL
 
 :MakeDirectories
-MKDIR %BIN_PATH%     %QUIET%
-MKDIR %BIN_PATH_X86% %QUIET%
-MKDIR %BIN_PATH_X64% %QUIET%
-GOTO :EOF
+  MKDIR %BIN_PATH%     %QUIET%
+  MKDIR %BIN_PATH_X86% %QUIET%
+  MKDIR %BIN_PATH_X64% %QUIET%
+  GOTO :EOF
 
 :Clean
-DEL %BIN_PATH_X86%\*.exp %QUIET%
-DEL %BIN_PATH_X64%\*.exp %QUIET%
-GOTO :EOF
+  DEL %BIN_PATH_X86%\*.exp %QUIET%
+  DEL %BIN_PATH_X64%\*.exp %QUIET%
+  GOTO :EOF
 
 :ExportsNotFound
-debug_error.cmd %*
-GOTO :EOF
+  debug_error.cmd %*
+  GOTO :EOF
